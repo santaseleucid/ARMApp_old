4d60963fadebd0a6e278848750d1bddd5aa4f43f
Sanjay <sanjay.abraham@hotmail.com>
2019-04-09 19:29:47 -0500

    fixing bugs

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,7 +21,7 @@ module.exports = (app) =>{
         '/auth/google/callback', 
         passport.authenticate('google'),
         (req, res) => {
-            res.redirect('/surveys'); 
+            res.redirect('/'); 
         }); 
 
 //logout user
