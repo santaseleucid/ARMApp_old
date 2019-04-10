c315995f14b0bf638b121f0c3d06679be07b6890
Sanjay <sanjay.abraham@hotmail.com>
2019-04-09 22:05:52 -0500

    react router bug fix -2

--- index.js
+++ index.js
@@ -41,12 +41,9 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('./client/build')); 
 
     const path = require('path'); 
-    app.get('/*', (req,res)=>{
-        res.sendFile(path.resolve(__dirnname, 'client/build/index.html'), function(err){
-            if(err){
-                res.status(500).send(err)
-            }
-        });
+    console.log(path); 
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirnname+'client/build/index.html'));
     });
 }
 
